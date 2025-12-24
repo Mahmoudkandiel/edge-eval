5e953667e77c63ac5b1f01faee8731bc902a384e
Mahmoud Kandiel <mahmoudkandiel8@gmail.com>
2025-12-24 12:03:23 +0200

    .

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import sophon.sail as sail
 
 
 if __name__ == '__main__':
-    bmodel_path = "yolov8n_1684_f32.bmodel"
+    dev_id = 0
+    print("dev {} tpu-util is {} %".format(dev_id,sail.get_tpu_util(dev_id)))
+    bmodel_path = "yolov8n_1684x_f16.bmodel"
     engine = sail.Engine(bmodel_path,0,sail.IOMode.SYSI)
     graph_name = engine.get_graph_names()[0]
     print("graph_name:",graph_name)
